bba72dbffc045915be045b7f66eaa7291debcce0
John Cionci <john@thinkoomph.com>
2014-07-24 10:51:14 -0400

    - update initial sass structure - add normalize as our reset - add js structure - initial pass at block, node, and .info

---
+++ templates/block.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a block.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728246
+ */
+?>
+<aside id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php if ($title): ?>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  <?php endif; ?>
+
+  <?php print $content; ?>
+
+</aside>
