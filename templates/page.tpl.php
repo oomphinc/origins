113d0ba9a9f2cf1c59148462f020030f9929e46f
Robert Aubin <rob@thinkoomph.com>
2014-08-01 17:15:49 -0400

    updating .info and templates. HTML5-ifying them

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Default theme implementation to display a single Drupal page.
+ * Oomphbase theme implementation to display a single Drupal page.
  *
  * The doctype, html, head and body tags are not in this template. Instead they
  * can be found in the html.tpl.php template in this directory.
@@ -29,6 +29,8 @@
  *   in theme settings.
  *
  * Navigation:
+ *  // TODO: we removed these featured, so maybe update docs?
+ *
  * - $main_menu (array): An array containing the Main menu links for the
  *   site, if they have been configured.
  * - $secondary_menu (array): An array containing the Secondary menu links for
@@ -57,7 +59,7 @@
  *
  * Regions:
  * - $page['help']: Dynamic help text, mostly for admin pages.
- * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['nav']: Should be reserved for navigation items.
  * - $page['content']: The main content of the current page.
  * - $page['sidebar_first']: Items for the first sidebar.
  * - $page['sidebar_second']: Items for the second sidebar.
@@ -75,7 +77,7 @@
 
   <div id="page-wrapper"><div id="page">
 
-    <div id="header"><div class="section clearfix">
+    <header id="header"><section class="section clearfix">
 
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
@@ -105,7 +107,8 @@
 
       <?php print render($page['header']); ?>
 
-    </div></div> <!-- /.section, /#header -->
+    </section>
+    </header> <!-- /.section, /#header -->
 
     <?php if ($page['nav']): ?>
       <nav class="nav">
@@ -113,50 +116,53 @@
       </nav>
     <?php endif; ?>
 
-    <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-      </div></div> <!-- /.section, /#navigation -->
-    <?php endif; ?>
-
     <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+      <nav id="breadcrumb"><?php print $breadcrumb; ?></nav>
     <?php endif; ?>
 
     <?php print $messages; ?>
 
-    <div id="main-wrapper"><div id="main" class="clearfix">
+    <div id="main-wrapper">
+    <main id="main" class="clearfix">
 
-      <div id="content" class="column"><div class="section">
-        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+      <div id="content" class="column"><section class="section">
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
         <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+
+        <?php if ($tabs): ?><nav class="tabs"><?php print render($tabs); ?></nav><?php endif; ?>
+
         <?php print render($page['help']); ?>
+
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+
+        <?php if ($page['content_top']): ?><?php print render($page['content_top']); ?><?php endif; ?>
         <?php print render($page['content']); ?>
+        <?php if ($page['content_bottom']): ?><?php print render($page['content_bottom']); ?><?php endif; ?>
+
         <?php print $feed_icons; ?>
-      </div></div> <!-- /.section, /#content -->
+      </section></div> <!-- /.section, /#content -->
 
       <?php if ($page['sidebar_first']): ?>
-        <div id="sidebar-first" class="column sidebar"><div class="section">
+        <div id="sidebar-first" class="column sidebar"><section class="section">
           <?php print render($page['sidebar_first']); ?>
-        </div></div> <!-- /.section, /#sidebar-first -->
+        </section></div> <!-- /.section, /#sidebar-first -->
       <?php endif; ?>
 
       <?php if ($page['sidebar_second']): ?>
-        <div id="sidebar-second" class="column sidebar"><div class="section">
+        <div id="sidebar-second" class="column sidebar"><section class="section">
           <?php print render($page['sidebar_second']); ?>
-        </div></div> <!-- /.section, /#sidebar-second -->
+        </section></div> <!-- /.section, /#sidebar-second -->
       <?php endif; ?>
 
-    </div></div> <!-- /#main, /#main-wrapper -->
+    </main>
+    </div> <!-- /#main, /#main-wrapper -->
 
-    <div id="footer"><div class="section">
-      <?php print render($page['footer']); ?>
-    </div></div> <!-- /.section, /#footer -->
+    <footer id="footer">
+      <section class="section">
+        <?php print render($page['footer']); ?>
+      </section>
+    </footer> <!-- /.section, /#footer -->
 
   </div></div> <!-- /#page, /#page-wrapper -->
